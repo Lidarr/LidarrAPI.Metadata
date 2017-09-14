5a211fdd11715b79f3efb4f85218c9a6323bd85c
Daniel Underwood <daniel.underwood13@gmail.com>
2017-09-13 20:05:18 -0400

    Update release script to join on artist credit name to fix some albums not being found

--- sql/album_search_artist_mbid.sql
+++ sql/album_search_artist_mbid.sql
@@ -1,11 +1,13 @@
-SELECT release.name as album,
-  release.gid as gid,
-  area.name as country,
-  release_country.date_day as day,
-  release_country.date_month as month,
-  release_country.date_year as year
+SELECT
+  release.name               AS album,
+  release.gid                AS gid,
+  area.name                  AS country,
+  release_country.date_day   AS day,
+  release_country.date_month AS month,
+  release_country.date_year  AS year
 FROM release
-  JOIN release_country on release.id = release_country.release
-  JOIN area on release_country.country = area.id
-  JOIN artist on release.artist_credit = artist.id
-  WHERE artist.gid = %s AND area.name = %s
+  JOIN release_country ON release.id = release_country.release
+  JOIN area ON release_country.country = area.id
+  JOIN artist_credit_name ON artist_credit_name.artist_credit = release.artist_credit
+  JOIN artist ON artist_credit_name.artist = artist.id
+WHERE artist.gid = %s AND area.name = %s
