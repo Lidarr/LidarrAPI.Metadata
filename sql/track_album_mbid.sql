6d0c280ac1e6bde3995b10ad85e00d52b6e29e22
Daniel Underwood <daniel.underwood13@gmail.com>
2017-09-19 21:03:25 -0400

    Make track query return tracks where artist credit is weird

--- sql/track_album_mbid.sql
+++ sql/track_album_mbid.sql
@@ -8,6 +8,6 @@ FROM track
   JOIN medium ON track.medium = medium.id
   JOIN release ON medium.release = release.id
   JOIN release_group ON release.release_group = release_group.id
-  JOIN artist ON release_group.artist_credit = artist.id
+  LEFT JOIN artist ON release_group.artist_credit = artist.id
   JOIN recording ON track.recording = recording.id
 WHERE release.gid = %s
