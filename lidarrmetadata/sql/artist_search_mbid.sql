fe1e43c8f63901a51ef96504dc92418b7cdf2d76
Daniel Underwood <daniel.underwood13@gmail.com>
2017-10-29 16:55:15 -0400

    Fix missing/wrong sql files

--- lidarrmetadata/sql/artist_search_mbid.sql
+++ lidarrmetadata/sql/artist_search_mbid.sql
@@ -1,6 +1,9 @@
 SELECT
-  id,
-  gid,
-  name
+  artist.gid,
+  artist.name,
+  ended,
+  comment,
+  artist_type.name AS type
 FROM artist
-WHERE gid = %s
+  JOIN artist_type ON artist.type = artist_type.id
+WHERE artist.gid = %s
