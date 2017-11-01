6d3a4dd99fcf0cb8e41df155931510fa5c5e9ca1
Daniel Underwood <daniel.underwood13@gmail.com>
2017-11-01 19:53:45 -0400

    Update track sql

--- lidarrmetadata/sql/track_album_mbid.sql
+++ lidarrmetadata/sql/track_album_mbid.sql
@@ -5,11 +5,10 @@ SELECT
   track.position,
   track.number,
   track.length,
-  release.gid,
-  medium.position AS medium_position,
+  medium.position    AS medium_position,
   medium_format.name AS medium_format
 FROM track
   JOIN medium ON track.medium = medium.id
   JOIN release ON medium.release = release.id
   JOIN medium_format ON medium.format = medium_format.id
-  WHERE release.gid = %s
+WHERE release.gid = % s
