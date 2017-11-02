2ec99cb0dcb82b890a7db618c242ada90ba6131b
Daniel Underwood <daniel.underwood13@gmail.com>
2017-11-01 22:47:57 -0400

    Add missing sql file

---
+++ lidarrmetadata/sql/media_album_mbid.sql
@@ -0,0 +1,8 @@
+SELECT
+  medium.name        AS medium_name,
+  medium.position    AS medium_position,
+  medium_format.name AS medium_format
+FROM medium
+  JOIN medium_format ON medium.format = medium_format.id
+  JOIN release ON medium.release = release.id
+WHERE release.gid = %s
