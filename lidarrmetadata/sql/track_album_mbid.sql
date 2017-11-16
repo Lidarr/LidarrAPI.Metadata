506f55172865f5920a8b56adad7114d7f23ecd05
Daniel Underwood <daniel.underwood13@gmail.com>
2017-11-16 18:20:07 -0500

    Remove unnecessary fields from track query

--- lidarrmetadata/sql/track_album_mbid.sql
+++ lidarrmetadata/sql/track_album_mbid.sql
@@ -5,9 +5,7 @@ SELECT
   track.position,
   track.number,
   track.length,
-  medium.name        AS medium_name,
-  medium.position    AS medium_position,
-  medium_format.name AS medium_format
+  medium.position    AS medium_position
 FROM track
   JOIN medium ON track.medium = medium.id
   JOIN release ON medium.release = release.id
