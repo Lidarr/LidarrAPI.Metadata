09719952a1aa1baf34af8e709892200e40202263
Daniel Underwood <daniel.underwood13@gmail.com>
2017-11-16 18:15:57 -0500

    Correct medium query

--- lidarrmetadata/sql/media_album_mbid.sql
+++ lidarrmetadata/sql/media_album_mbid.sql
@@ -3,6 +3,6 @@ SELECT
   medium.position    AS medium_position,
   medium_format.name AS medium_format
 FROM medium
-  JOIN medium_format ON medium.format = medium_format.id
+  FULL JOIN medium_format ON medium.format = medium_format.id
   JOIN release ON medium.release = release.id
 WHERE release.gid = %s
