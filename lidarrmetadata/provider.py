676de0cd02341c2f0f5e068938e1443160e1ab89
Daniel Underwood <daniel.underwood13@gmail.com>
2017-11-29 21:51:01 -0500

    Rework wikipedia URL parsing to handle additional cases

--- lidarrmetadata/provider.py
+++ lidarrmetadata/provider.py
@@ -664,6 +664,8 @@ class WikipediaProvider(Provider, ArtistOverviewMixin):
     Provider for querying wikipedia
     """
 
+    URL_REGEX = re.compile(r'https?://\w+\.wikipedia\.org/wiki/(?P<title>.+)')
+
     def __init__(self):
         """
         Class initialization
@@ -691,14 +693,13 @@ class WikipediaProvider(Provider, ArtistOverviewMixin):
             logger.error('Wikipedia DisambiguationError from {url}: {e}'.format(e=error, url=url))
             return ''
 
-    @staticmethod
-    def title_from_url(url):
+    @classmethod
+    def title_from_url(cls, url):
         """
         Gets the wikipedia page title from url. This may not work for URLs with
         certain special characters
         :param url: URL of wikipedia page
         :return: Title of page at URL
         """
-        page = url.split('/')[-1]
-        encoded_title = page.replace('_', ' ')
-        return urllib.unquote(encoded_title)
+        title = cls.URL_REGEX.match(url).group('title')
+        return urllib.unquote(title)
