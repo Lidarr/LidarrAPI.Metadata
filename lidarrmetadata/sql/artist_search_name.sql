933de20bed88853b414cdf74093022007df7d0a8
Daniel Underwood <daniel.underwood13@gmail.com>
2018-04-01 22:24:18 -0400

    Order by musicbrainz ratings

--- lidarrmetadata/sql/artist_search_name.sql
+++ lidarrmetadata/sql/artist_search_name.sql
@@ -2,7 +2,14 @@ SELECT
   artist.gid,
   artist.name,
   comment,
-  artist_type.name AS type
+  artist_type.name AS type,
+  artist_meta.rating,
+  artist_meta.rating_count
 FROM artist
   LEFT JOIN artist_type ON artist.type = artist_type.id
+  LEFT JOIN artist_meta ON artist.id = artist_meta.id
 WHERE UPPER(artist.name) LIKE UPPER(%s)
+ORDER BY
+  CASE WHEN artist_meta.rating_count IS NULL THEN 1 ELSE 0 END,
+  artist_meta.rating_count DESC,
+  artist_meta.rating DESC
