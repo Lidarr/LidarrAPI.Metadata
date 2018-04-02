e25264e64fbf266486c6837b53f101b5cb94a0be
Daniel Underwood <daniel.underwood13@gmail.com>
2018-04-01 22:38:14 -0400

    Order search results to give exact matches first

--- lidarrmetadata/sql/artist_search_name.sql
+++ lidarrmetadata/sql/artist_search_name.sql
@@ -10,6 +10,7 @@ FROM artist
   LEFT JOIN artist_meta ON artist.id = artist_meta.id
 WHERE UPPER(artist.name) LIKE UPPER(%s)
 ORDER BY
+  CASE WHEN UPPER(artist.name) = UPPER(%s) THEN 0 ELSE 1 END,
   CASE WHEN artist_meta.rating_count IS NULL THEN 1 ELSE 0 END,
   artist_meta.rating_count DESC,
   artist_meta.rating DESC
