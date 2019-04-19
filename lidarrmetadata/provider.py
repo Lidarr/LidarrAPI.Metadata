4e891d9790a9f23167dfb3d78aa7ad9aa0b030e3
Daniel Underwood <daniel.underwood13@gmail.com>
2019-04-19 14:51:47 -0400

    Fix wikipedia PageError logging

--- lidarrmetadata/provider.py
+++ lidarrmetadata/provider.py
@@ -818,7 +818,7 @@ class WikipediaProvider(Provider, ArtistOverviewMixin):
                 return self._client.summary(title, auto_suggest=False)
         # FIXME Both of these may be recoverable
         except mediawikiapi.PageError as error:
-            logger.error(u'Wikipedia PageError from {title}: {e}' % {'e':error, 'title':title})
+            logger.error(u'Wikipedia PageError from {title}: {e}'.format(e=error, title=title))
             return ''
         except ValueError as error:
             logger.error(u'Page parse error: {e}'.format(e=error))
