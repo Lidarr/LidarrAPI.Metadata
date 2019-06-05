826a84c67c59b14af9860c9eb5042f325244b0d6
ta264 <ta264@users.noreply.github.com>
2019-06-05 21:50:25 +0100

    Format update queries and capture merged artists/albums

--- lidarrmetadata/sql/updated_albums.sql
+++ lidarrmetadata/sql/updated_albums.sql
@@ -1,37 +1,43 @@
 SELECT release_group.gid
-FROM release_group
-WHERE release_group.last_updated > $1
+  FROM release_group
+ WHERE release_group.last_updated > $1
 
-UNION
+ UNION
 
-SELECT DISTINCT release_group.gid
-FROM release_group
-JOIN release on release.release_group = release_group.id
-WHERE release.last_updated > $1
+SELECT gid
+  FROM release_group_gid_redirect
+ WHERE created > $1
 
-UNION
-
-SELECT DISTINCT release_group.gid
-FROM release_group
-JOIN release on release.release_group = release_group.id
-JOIN medium on medium.release = release.id
-WHERE medium.last_updated > $1
-
-UNION
+ UNION
 
 SELECT DISTINCT release_group.gid
-FROM release_group
-JOIN release ON release.release_group = release_group.id
-JOIN medium ON medium.release = release.id
-JOIN track ON track.medium = medium.id
-WHERE track.last_updated > $1
+  FROM release_group
+         JOIN release on release.release_group = release_group.id
+ WHERE release.last_updated > $1
 
-UNION
+ UNION
 
 SELECT DISTINCT release_group.gid
-FROM release_group
-JOIN release ON release.release_group = release_group.id
-JOIN medium ON medium.release = release.id
-JOIN track ON track.medium = medium.id
-JOIN recording ON track.recording = recording.id
-WHERE recording.last_updated > $1
+  FROM release_group
+         JOIN release on release.release_group = release_group.id
+         JOIN medium on medium.release = release.id
+ WHERE medium.last_updated > $1
+
+ UNION
+
+SELECT DISTINCT release_group.gid
+  FROM release_group
+         JOIN release ON release.release_group = release_group.id
+         JOIN medium ON medium.release = release.id
+         JOIN track ON track.medium = medium.id
+ WHERE track.last_updated > $1
+
+ UNION
+
+SELECT DISTINCT release_group.gid
+  FROM release_group
+         JOIN release ON release.release_group = release_group.id
+         JOIN medium ON medium.release = release.id
+         JOIN track ON track.medium = medium.id
+         JOIN recording ON track.recording = recording.id
+ WHERE recording.last_updated > $1
