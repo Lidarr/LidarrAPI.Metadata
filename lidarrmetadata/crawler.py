dd7b4eb5ad3f2c37bfd87c2172d19482dce55edc
ta264 <ta264@users.noreply.github.com>
2019-06-08 21:28:29 +0100

    Rework error handling

--- lidarrmetadata/crawler.py
+++ lidarrmetadata/crawler.py
@@ -143,7 +143,11 @@ async def update_albums(count = 100, max_ttl = 60 * 60):
             start = timer()
             results = await get_release_group_info_multi(keys)
             
-            missing_albums = set(keys) - set(rg['id'] for rg, _ in results)
+            if not results:
+                missing_albums = keys
+            else:
+                missing_albums = set(keys) - set(rg['id'] for rg, _ in results)
+                
             if missing_albums:
                 logger.debug(f"Removing deleted albums:\n{missing_albums}")
                 await asyncio.gather(*(util.ALBUM_CACHE.delete(id) for id in missing_albums))
