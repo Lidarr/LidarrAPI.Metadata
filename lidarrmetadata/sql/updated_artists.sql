e32fa95e2cda21266238f7c8821a71befe4d83dc
ta264 <ta264@users.noreply.github.com>
2019-09-08 18:11:24 +0100

    Update artist cache if any releases have updated

--- lidarrmetadata/sql/updated_artists.sql
+++ lidarrmetadata/sql/updated_artists.sql
@@ -13,6 +13,16 @@ SELECT DISTINCT artist.gid
  WHERE artist_credit_name.position = 0
    AND release_group.last_updated > $1
 
+       UNION
+
+  -- artist release updated (release group status is calculated from releases and returned in artist query)
+SELECT DISTINCT artist.gid
+  FROM artist
+         JOIN artist_credit_name ON artist_credit_name.artist = artist.id
+         JOIN release_group ON release_group.artist_credit = artist_credit_name.artist_credit
+         JOIN release ON release.release_group = release_group.id
+ WHERE artist_credit_name.position = 0
+   AND release.last_updated > $1
 
        UNION
        
