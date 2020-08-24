7cc2fca1f912c0a108e622a9dc4034a1328a0893
ta264 <ta264@users.noreply.github.com>
2020-08-24 21:41:14 +0100

    Fix spotify mapping by album url

--- lidarrmetadata/sql/all_spotify_maps.sql
+++ lidarrmetadata/sql/all_spotify_maps.sql
@@ -14,6 +14,15 @@ JOIN l_release_group_url ON l_release_group_url.entity0 = release_group.id
 JOIN url ON l_release_group_url.entity1 = url.id
 WHERE url.url LIKE 'https://open.spotify.com/album/%'
 
+UNION
+
+SELECT release_group.gid as mbid, substring(url.url from 32) as spotifyid
+FROM release_group
+JOIN release ON release.release_group = release_group.id
+JOIN l_release_url ON l_release_url.entity0 = release.id
+JOIN url ON l_release_url.entity1 = url.id
+WHERE url.url LIKE 'https://open.spotify.com/album/%'
+
 )
 
 SELECT DISTINCT ON (spotifyid) spotifyid, mbid
--- lidarrmetadata/sql/release_group_id_from_spotify_id.sql
+++ lidarrmetadata/sql/release_group_id_from_spotify_id.sql
@@ -1,5 +1,7 @@
 SELECT release_group.gid
 FROM release_group
-JOIN l_release_group_url ON l_release_group_url.entity0 = release_group.id
-JOIN url ON l_release_group_url.entity1 = url.id
+JOIN release ON release.release_group = release_group.id
+JOIN l_release_url ON l_release_url.entity0 = release.id
+JOIN url ON l_release_url.entity1 = url.id
 WHERE url.url = 'https://open.spotify.com/album/' || $1
+LIMIT 1
